115801cac5c6de7c1203b48ac831187a8e54e687
shyamrendra <shyamrendra@gmail.com>
2019-12-21 20:11:41 +0530

    Updates

---
+++ precache-manifest.0aa0f872c5ad5d733577497378b660f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65bbfa92c8d1c3b461aa347c43b4cd8a",
+    "url": "/gitapp/index.html"
+  },
+  {
+    "revision": "9184fe37bb01fd76b846",
+    "url": "/gitapp/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5c5df7ee0b0ace0b0ea7",
+    "url": "/gitapp/static/js/2.696b3442.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/gitapp/static/js/2.696b3442.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9184fe37bb01fd76b846",
+    "url": "/gitapp/static/js/main.a3d4218e.chunk.js"
+  },
+  {
+    "revision": "93c49d9d6e5ed9379379",
+    "url": "/gitapp/static/js/runtime-main.d70711ef.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gitapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
